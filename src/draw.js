91e030fdc4420d2f0047a617e36f8cc17ed048a5
jules marchand <friarobaz@gmail.com>
2022-06-04 23:06:37 +0200

    nice

--- src/draw.js
+++ src/draw.js
@@ -13,8 +13,9 @@ const drawVerticalLine = (ctx, x, yMax) => {
   ctx.stroke()
 }
 
-export const drawGrid = (ctx, height, width, step, color = "black") => {
+const drawGrid = (ctx, height, width, step, color = "black") => {
   ctx.strokeStyle = color
+  ctx.lineWidth = 1
 
   for (let x = 0; x < width; x += step) {
     drawVerticalLine(ctx, x, height)
@@ -24,19 +25,23 @@ export const drawGrid = (ctx, height, width, step, color = "black") => {
   }
 }
 
-export const circle = (x, y, ctx, color = "red", radius = 3) => {
+const circle = (x, y, ctx, color = "red", radius = 3) => {
   ctx.fillStyle = color
   ctx.beginPath()
   ctx.arc(x, y, radius, 0, 2 * Math.PI)
   ctx.fill()
 }
 
-export const findPoints = (
-  myFunction,
-  start = 0,
-  end = 1500,
-  precision = 0.2
-) => {
+const cursorHelper = (x, y, ctx) => {
+  ctx.fillStyle = "red"
+  ctx.fillRect(0, y - 2, 10, 4)
+  ctx.fillRect(x - 2, 0, 4, 10)
+  ctx.strokeStyle = "black"
+  ctx.strokeText(`${x}`, x + 15, 10)
+  ctx.strokeText(`${y}`, 15, y + 10)
+}
+
+const findPoints = (myFunction, start = 0, end = 1500, precision = 0.2) => {
   let points = []
   for (let i = start; i < end; i += precision) {
     points.push({ x: i, y: myFunction(i) })
@@ -49,10 +54,18 @@ export const findPoints = (
  * @param points
  * @param {string} color
  */
-const drawPoints = (ctx, points, color = "red") => {
+const drawCurve = (
+  ctx,
+  points,
+  color = "red",
+  lineWidth = 2,
+  showPoints = false
+) => {
   ctx.strokeStyle = color
-  for (const point of points) {
-    circle(point.x, point.y, ctx, color)
+  if (showPoints) {
+    for (const point of points) {
+      circle(point.x, point.y, ctx, color)
+    }
   }
   ctx.beginPath()
   ctx.moveTo(points[0].x, points[0].y)
@@ -60,7 +73,7 @@ const drawPoints = (ctx, points, color = "red") => {
   for (const point of points) {
     ctx.lineTo(point.x, point.y)
   }
-
+  ctx.lineWidth = lineWidth
   ctx.stroke()
 }
 
@@ -68,13 +81,19 @@ export const draw = (ctx, settings) => {
   ctx.clearRect(0, 0, settings.width, settings.height)
   drawGrid(ctx, settings.height, settings.width, 10, "rgba(0, 100, 255, 0.15)")
   drawGrid(ctx, settings.height, settings.width, 50, "rgba(255, 150, 0, 0.4)")
-  ctx.fillStyle = "red"
-  ctx.fillRect(0, settings.mouseY - 2, 10, 4)
-  ctx.fillRect(settings.mouseX - 2, 0, 4, 10)
-  ctx.strokeStyle = "black"
-  ctx.strokeText(`${settings.mouseX}`, settings.mouseX + 15, 10)
-  ctx.strokeText(`${settings.mouseY}`, 15, settings.mouseY + 10)
+  cursorHelper(settings.mouseX, settings.mouseY, ctx)
 
+  let testFunction = (i) => {
+    return Math.cos((i - 50) / 80) * 50 + 100 + i / 3
+  }
+  let curvePoints = findPoints(testFunction, 150, 450, 10)
+  drawCurve(ctx, curvePoints, "blue", 5)
+  if (settings.mouseX < 450 && settings.mouseX > 150) {
+    circle(settings.mouseX, testFunction(settings.mouseX), ctx, "red")
+  }
+}
+
+const rien = (ctx) => {
   const points = [
     { x: 20, y: 1.5 },
     { x: 30, y: 1 },
@@ -85,15 +104,6 @@ export const draw = (ctx, settings) => {
   const pointsHeavy = points.map((p) => ({ x: p.x * 2, y: p.y * 2 }))
   drawPoints(ctx, points)
   drawPoints(ctx, pointsHeavy, "green")
-  let testFunction = (i) => {
-    return Math.cos((i - 50) / 80) * 50 + 100 + i / 3
-  }
-  let curvePoints = findPoints(testFunction, 150, 450, 3)
-  drawPoints(ctx, curvePoints, "blue")
-  if (settings.mouseX < 450 && settings.mouseX > 150) {
-    circle(settings.mouseX, testFunction(settings.mouseX), ctx, "red")
-  }
-
   ctx.beginPath()
   ctx.strokeStyle = "blue"
   ctx.moveTo(0, 0)
